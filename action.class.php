4fd4c299cbe71269f19438a81603ce4d7074ced8
costlund <claes@webforms.se>
2016-12-31 09:59:11 +0100

    Documentation

--- action.class.php
+++ action.class.php
@@ -14,24 +14,38 @@ class PluginWfI18n{
   #code#
    */
   public static function event_translate($value, $element){
+    /**
+     * Get language settings.
+     */
     $language = wfArray::get($GLOBALS, 'sys/settings/i18n/language');
     if(!$language){
       return $element;
     }
+    /**
+     * Check if element not to be translated.
+     */
     if(wfArray::issetAndTrue($element, 'settings/i18n/exclude')){
       return $element;
     }
-    // If key innerHTML is set.
+    /**
+     * Check if key innerHTML exist.
+     */
     if(wfArray::isKey($element, 'innerHTML')){
-      // And not an array.
+      /**
+       * innerHTML must be a string.
+       */
       if(!is_array(wfArray::get($element, 'innerHTML'))){
-        // Get content to do something with?
+        /**
+         * Pick up innerHTML, can also be a separate value in element settings.
+         */
         if(wfArray::get($element, 'settings/i18n/key')){
           $innerHTML = wfArray::get($element, 'settings/i18n/key');
         }else{
           $innerHTML = wfArray::get($element, 'innerHTML');
         }
-        
+        /**
+         * From element.
+         */
         if(wfArray::get($element, 'settings/i18n/plugin')){
           $filename = '/theme/[theme]/i18n/plugin/'.wfArray::get($element, 'settings/i18n/plugin').'/'.$language.'.yml';
           if(wfFilesystem::fileExist(wfArray::get($GLOBALS, 'sys/app_dir').$filename)){
@@ -52,6 +66,9 @@ class PluginWfI18n{
             }
           }
         }
+        /**
+         * From theme.
+         */
         $filename = '/theme/[theme]/i18n/'.$language.'.yml';
         if(wfFilesystem::fileExist(wfArray::get($GLOBALS, 'sys/app_dir').$filename)){
           $temp = wfSettings::getSettings($filename);
@@ -61,11 +78,47 @@ class PluginWfI18n{
             return $element;
           }
         }
+//        $i18n = new PluginWfI18n();
+//        return $i18n->translate('/theme/[theme]/i18n/'.$language.'.yml', $element, $innerHTML);
+        
       }
     }
     return $element;
   }
+  /**
+   * 
+   * @param string $innerHTML Just any string.
+   * @return string
+   */
+  public function translateFromTheme($innerHTML, $replace = null){
+    /**
+     * Retreive language.
+     */
+    $language = wfArray::get($GLOBALS, 'sys/settings/i18n/language');
+    if($language){
+      /**
+       * Check from theme.
+       */
+      $filename = '/theme/[theme]/i18n/'.$language.'.yml';
+      if(wfFilesystem::fileExist(wfArray::get($GLOBALS, 'sys/app_dir').$filename)){
+        $temp = wfSettings::getSettings($filename);
+        if($temp && isset($temp[$innerHTML])){
+          $innerHTML = $temp[$innerHTML];
+        }
+      }
+    }
+    /**
+     * Replace.
+     */
+    if($replace){
+      foreach ($replace as $key => $value) {
+        $innerHTML = str_replace($key, $value, $innerHTML);
+      }
+    }
+    return $innerHTML;
+  }
 }
+
 
 
 
